f7028a01d2601fbebdd822b5e689a5ce487aa92e
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-09-23 00:41:23 +0600

    portfolio left

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Qualification from "./components/qualification/Qualification";
+import ScrollUp from "./components/scrollup/ScrollUp";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
 import Testimonials from "./components/testimonials/Testimonials";
+import Work from "./components/work/Work";
 
 function App() {
   return (
@@ -18,9 +21,12 @@ function App() {
         <Skills></Skills>
         <Services></Services>
         <Qualification></Qualification>
+        <Work></Work>
         <Testimonials></Testimonials>
         <Contact></Contact>
       </main>
+      <Footer></Footer>
+      <ScrollUp></ScrollUp>
     </>
   );
 }
